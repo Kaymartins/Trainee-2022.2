db19e5216f3cbde023f9cf2cc405a1605590fedd
Ssssspook <carlosgustavo.rezende@estudante.ufjf.br>
2022-11-27 23:26:47 -0300

    criando as funções do controller para editar e excluir e configurando os forms para pegar do banco de dados

--- JornalDeKonoha/app/Controllers/PostController.php
+++ JornalDeKonoha/app/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 
 use App\Models\Post;
 use App\Core\App;
+use DateTime;
 
 class PostController extends Controller
 {
@@ -21,78 +22,49 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::all();
+        foreach ($posts as $post):
+        $datapost = new DateTime($post->date);
+        $post->date = $datapost->format("d/m/Y");
+        endforeach;
         return view('admin/lista_de_postagens', compact('posts'));
     }
 
     public function create()
     {
-        //return view('...');
-    }
+        $titulo = filter_input(INPUT_POST, 'titulo', FILTER_SANITIZE_SPECIAL_CHARS);
+        $date = filter_input(INPUT_POST, 'date', FILTER_SANITIZE_SPECIAL_CHARS);
+        $conteudo = filter_input(INPUT_POST, 'conteudo', FILTER_SANITIZE_SPECIAL_CHARS);
+        $imagem = filter_input(INPUT_POST, 'imagem', FILTER_SANITIZE_SPECIAL_CHARS);
 
-    public function show()
-    {
-        //$id = "validação da variavel global $_GET no indice que você quiser. Por exemplo $_GET['id']. Preferenciamentel coloque o campo de identificação do usuario com o nome de id"
-        //$exemplo = App\Models\Exemplo::find($id);
-        //return view('...', compact("exemplo"))
-    }
-
-    //retorna a pagina responsavel por criar um elemento
-
-    // valida e armazena os dados preenchidos no front e redireciona para alguma rota caso tudo esteja ok, caso contrario redireciona para a pagina anterior com alguma mensagem de erro
-    public function store()
-    {
-        //Exemplo para o registro de um usuario
-
-        /*$filterForm = [
-            "name" => FILTER_SANITIZE_STRIPPED,
-            "email" => FILTER_VALIDATE_EMAIL,
-            "password" => FILTER_SANITIZE_STRIPPED,
-            "birthdate" => FILTER_SANITIZE_STRIPPED,
-            "gender" => FILTER_SANITIZE_STRIPPED,
-        ];*/
-
-        //$userData = filter_input_array(INPUT_POST, $filterForm);
-
-        /*if(in_array(false, $userData)) {
-            $errors = array_keys($userData, false, false);
-            $_SESSION["errors"] = [];
-            foreach($errors as $error) {
-                $_SESSION["errors"][$error] = "Coloque sua mensagem de erro";
-            }
-            return view('...');
-        }*/
-
-        /*try {
-            $userData["password"] = password_hash($userData["password"], PASSWORD_BCRYPT);
-            $user = User::create($userData);
-        } catch(QueryException $PDOException) {
-            $_SESSION["error"] = ["email" => "Email já foi cadastrado"];
-            return view('guests/register_page');
+        if(!$titulo) {
+            $_SESSION['faltaCampos'] = 'ERRO: preencha o campo titulo!';
+            redirect('posts');
+            exit();
         }
-        unset($_SESSION["error"]);
-        $_SESSION["logado"] = $user->getAttributes();
-        return redirect('...');*/
+
+        App::get('database')->adicionar('posts', compact('titulo', 'date', 'conteudo', 'imagem'));
+
+        redirect('posts');
     }
 
-    // retorna a pagina para editar um elemento
-    public function edit()
-    {
-        //$id = "validação da variavel global $_GET no indice que você quiser. Por exemplo $_GET['id']. Preferenciamentel coloque o campo de identificação do usuario com o nome de id"
-        //$exemplo = App\Models\Exemplo::find($id);
-        //return view('...', compact("exemplo"))
-    }
-
-    // valida e atualiza os dados preenchidos no front e redireciona para alguma rota caso tudo esteja ok, caso contrario redireciona para a pagina anterior com alguma mensagem de erro
     public function update()
-    {
-        //Muito trampo escrever, esse aqui vocês dão uma pensada ai.
+    {   
+
+        $param  = [
+            'titulo' => $_POST['titulo'],
+            'date' => $_POST['date'],
+            'conteudo' => $_POST['conteudo'],
+            'imagem' => $_POST['imagem'],
+        ];
+        app::get('database')->edit($_POST['id'],'posts', $param);
+
+        redirect('posts');
     }
 
-    // deleta um elemento e redireciona para alguma rota
     public function delete()
     {
-        //$id = "validação da variavel global $_GET no indice que você quiser. Por exemplo $_GET['id'];"
-        //App\Models\Exemplo::destroy($id);
-        //return redirect('...');
+        $id = $_POST['id'];
+        App::get('database')->delete('posts', $id);
+        redirect('posts');
     }
 }
