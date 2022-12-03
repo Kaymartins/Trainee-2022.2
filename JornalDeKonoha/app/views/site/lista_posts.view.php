dfe438d288d490f5dbe3c661c178eee76f90c6a8
Joao-Vitor-Fernandes <joviramos16@gmail.com>
2022-12-02 21:28:44 -0300

    fazendo o segundo sistema de busca

--- JornalDeKonoha/app/views/site/lista_posts.view.php
+++ JornalDeKonoha/app/views/site/lista_posts.view.php
@@ -14,17 +14,20 @@
 //     header('Location: listaposts');
 // }
 
-$mysql = mysqli_connect('localhost', 'root', '', 'jornaldekonoha');
-$pesquisar = $_GET['busca'];
-//echo $pesquisar; 
 
-$result_posts = "SELECT * FROM posts WHERE titulo LIKE '%$pesquisar%'";
-$resultado_posts = mysqli_query($mysql, $result_posts);
 
-while($rows_cursos = mysqli_fetch_array($resultado_posts)) {
 
-    echo "Nome do post: " . $rows_cursos['titulo'] . "<br>";
-}
+// $mysql = mysqli_connect('localhost', 'root', '', 'jornaldekonoha');
+// $pesquisar = $_GET['busca'];
+// //echo $pesquisar; 
+
+// $result_posts = "SELECT * FROM posts WHERE titulo LIKE '%$pesquisar%'";
+// $resultado_posts = mysqli_query($mysql, $result_posts);
+
+// while($rows_cursos = mysqli_fetch_array($resultado_posts)) {
+
+//     echo "Nome do post: " . $rows_cursos['titulo'] . "<br>";
+// }
 
 
 ?>
@@ -53,13 +56,8 @@ while($rows_cursos = mysqli_fetch_array($resultado_posts)) {
                     <div class="container-fluid">
                         <form class="d-flex">
                             <input name="busca" class="form-control me-2" type="search" placeholder="Pesquisar posts"
-                                aria-label="Search">
-                            <button type="submit" class="btn btn-primary">&telrec;</button>
-                            <?php 
-                            // if(!isset($_GET['busca'])) {
-                            //     header('Location: listaposts');
-                            // }
-                            ?>
+                                aria-label="Search" id="pesquisar" value="<?php if(isset($_GET['busca'])) echo $_GET['busca']; ?>">
+                            <button type="submit" class="btn btn-primary" onclick="searchData()">&telrec;</button>
                         </form>
                     </div>
                 </nav>
@@ -67,8 +65,10 @@ while($rows_cursos = mysqli_fetch_array($resultado_posts)) {
 
             <div class="card-vertical">
                 <?php $cont = 0;
-                    foreach ($posts as $post): 
-                        if (++$cont <= 4) { ?>
+                //while($rows_cursos = mysqli_fetch_array($resultado_posts)):
+                if(!isset($_GET['busca'])) {
+                foreach ($posts as $post): 
+                    if (++$cont <= 4) { ?>
                 <div class="card cards-ver" style="width: 18rem;">
                     <img src="../../../public/img/<?= $post->imagem ?>" class="card-img-top" alt="imagem">
                     <div class="card-body">
@@ -97,11 +97,54 @@ while($rows_cursos = mysqli_fetch_array($resultado_posts)) {
                             </div>
                         </div>
                     </div>
-                <?php } endforeach; ?>
+                <?php } endforeach; 
+                    //endwhile;
+                } else{ 
+                ?>
+                    <?php       
+                    $mysqli = new mysqli('localhost', 'root', '', 'jornaldekonoha');
+
+                    $pesquisa = $mysqli->real_escape_string($_GET['busca']);
+                    $sql_code = "SELECT * FROM posts WHERE titulo LIKE '%$pesquisa%'";
+                    $sql_query = $mysqli->query($sql_code);
+                    if($sql_query->rum_rows > 0){
+                        while($dados = $sql_query->fetch_assoc()){
+                            foreach ($posts as $post):
+                        ?>
+                            <div class="card-horizontal">
+                                <div class="card mb-3 cards-hor">
+                                    <div class="row g-0">
+                                        <div class="col-md-4 imagem">
+                                            <img src="../../../public/img/<?= $post->imagem ?>" class="img-fluid rounded-start"
+                                                alt="imagem">
+                                        </div>
+                                        <div class="col-md-8">
+                                            <div class="card-body card-texto">
+                                                <h5 class="card-title"><?= $post->titulo ?></h5>
+                                                <p class="card-text"><small class="text-muted"><span><?= date('d/m/Y',strtotime($post->date)) ?></span></small></p>
+                                                <p class="card-text"><?= $post->conteudo ?></p>
+                                                <a class="mais" href="#">Leia mais >>></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php
+                        endforeach;
+                        }
+                    }
+                } ?>
             </div>
         </div>
 
         <?php require './app/views/include/footer.html'?>
+        
+        <script>
+            var search = document.getElementeById('pesquisar');
+            function searchData() {
+                window.location = 'listaposts?search=' + search.value;
+            }
+        </script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
             crossorigin="anonymous"></script>
