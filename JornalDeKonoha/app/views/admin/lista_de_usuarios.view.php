1fd7f5d9c3cd79155419490e7b823a26b4e640dd
Ssssspook <carlosgustavo.rezende@estudante.ufjf.br>
2022-12-09 21:33:09 -0300

    corrigindo o erro na sidebar, corrigindo as posições dos scripts que estavam no head das páginas

--- JornalDeKonoha/app/views/admin/lista_de_usuarios.view.php
+++ JornalDeKonoha/app/views/admin/lista_de_usuarios.view.php
@@ -8,12 +8,15 @@
         <title>Lista de Usuários</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-        <link rel="stylesheet" href="../../../public/css/lista_de_usuarios.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+        <link rel="stylesheet" href="../../../public/css/lista_de_usuarios.css">
+        <link rel="stylesheet" href="../../../public/css/sidebar.css">
     </head>
     
     <body>
         
+    <?php require './app/views/include/sidebar.html'?>
         <div class="navigation container">
             <h1 class="titulo"><u>Tabela-Usuários:</u></h1>
             <button type="button" class="btn btn-primary but-add" title="Adicionar Usuário" data-modal="modalAdd">
@@ -169,5 +172,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
     crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="../../../public/js/modalUsuario.js"></script>
 </html>
--- JornalDeKonoha/app/views/site/login.view.php
+++ JornalDeKonoha/app/views/site/login.view.php
@@ -8,7 +8,6 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../../../public/css/login.css">
     <link rel="stylesheet" href="../../../public/css/navbar_e_footer.css">
     <meta name="viewport" content="width=device-width, inicial-scale=1.0">
@@ -44,6 +43,10 @@
         </div>
     </div>
     <?php require './app/views/include/footer.html'?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
