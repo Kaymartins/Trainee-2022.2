87efa1f969a7d02d320a470f184440de3f0cb58d
Joao-Vitor-Fernandes <joviramos16@gmail.com>
2022-12-15 20:53:44 -0300

    limpando codigo UserControler

--- JornalDeKonoha/app/Controllers/UserController.php
+++ JornalDeKonoha/app/Controllers/UserController.php
@@ -61,78 +61,7 @@ class UserController extends Controller
 
         redirect('users');
     }
-
-    //retorna pagina individual de um elemento
-    public function show()
-    {
-        //$id = "validação da variavel global $_GET no indice que você quiser. Por exemplo $_GET['id']. Preferenciamentel coloque o campo de identificação do usuario com o nome de id"
-        //$exemplo = App\Models\Exemplo::find($id);
-        // //return view('...', compact("exemplo"))
-        
-        // $users = App::get('database')->selectAll();
-        // return view('admin/lista_de_usuarios');
-    }
-
-    // valida e armazena os dados preenchidos no front e redireciona para alguma rota caso tudo esteja ok, caso contrario redireciona para a pagina anterior com alguma mensagem de erro
-    public function store()
-    {
-        //Exemplo para o registro de um usuario
-
-        /*$filterForm = [
-            "name" => FILTER_SANITIZE_STRIPPED,
-            "email" => FILTER_VALIDATE_EMAIL,
-            "password" => FILTER_SANITIZE_STRIPPED,
-            "birthdate" => FILTER_SANITIZE_STRIPPED,
-            "gender" => FILTER_SANITIZE_STRIPPED,
-        ];*/
-
-        //$userData = filter_input_array(INPUT_POST, $filterForm);
-
-        /*if(in_array(false, $userData)) {
-            $errors = array_keys($userData, false, false);
-            $_SESSION["errors"] = [];
-            foreach($errors as $error) {
-                $_SESSION["errors"][$error] = "Coloque sua mensagem de erro";
-            }
-            return view('...');
-        }*/
-
-        /*try {
-            $userData["password"] = password_hash($userData["password"], PASSWORD_BCRYPT);
-            $user = User::create($userData);
-        } catch(QueryException $PDOException) {
-            $_SESSION["error"] = ["email" => "Email já foi cadastrado"];
-            return view('guests/register_page');
-        }
-        unset($_SESSION["error"]);
-        $_SESSION["logado"] = $user->getAttributes();
-        return redirect('...');*/
-    }
-
-    // retorna a pagina para editar um elemento
-    /*public function edit($id, $table, $param)
-    {
-        $sql = sprintf(
-            'UPDATE %s SET %s WHERE %s;',
-            $table,
-            implode(', ',array_map(function($param){
-                return "{$param} = :{$param}";
-            },array_keys($param))),
-            'id =:id'
-        );
-
-        $param['id'] = $id;
-
-        try{
-            $stmt = $this->pdo->prepare($sql);
-            $stmt->execute($param);
-        } catch(Exception $e){
-            die("Erro ao editar BD: {$e->getMessage()}");
-        }
-
-    }*/
-
-    // valida e atualiza os dados preenchidos no front e redireciona para alguma rota caso tudo esteja ok, caso contrario redireciona para a pagina anterior com alguma mensagem de erro
+    
     public function update()
     {   
 
